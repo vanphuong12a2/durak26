ec134283682b1bd458377ee8f320447c18b57f99
pnguyen <vanphuong12a2@gmail.com>
2018-12-28 00:44:37 +0100

    Add typescript & tslint to frontend

--- views/src/App.tsx
+++ views/src/App.tsx
@@ -2,9 +2,9 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-  render() {
+  public render() {
     return (
-      <div className="App">
+      <div className='App'>
         Hello, world!
       </div>
     );
