50e98602172ec18a982f4126fd0eebe8730befdc
pnguyen <vanphuong12a2@gmail.com>
2018-12-28 22:39:52 +0100

    Disable grunt completely

--- api/controllers/IndexController.js
+++ api/controllers/IndexController.js
@@ -5,7 +5,7 @@
  * @help        :: See https://sailsjs.com/docs/concepts/actions
  */
 
-const PATH_TO_PUBLIC = __dirname + '/../../.tmp/public';
+const PATH_TO_PUBLIC = __dirname + '/../../frontend/build';
 
 module.exports = {
   index: function(req, res) {
